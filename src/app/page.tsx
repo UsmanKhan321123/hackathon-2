0badae14515a7a6f092f37eb0314eaf658e8b1e0
UsmanKhan321123 <uk732161@gmail.com>
2024-12-25 13:23:10 +0500

    done

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import Hero from "../components/Hero";
+import About from "../components/About"
+import FoodCata from "../components/FoodCata" 
+import OurChefs from "./ourChefs/page";
+import Experience from "@/components/Experience";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import BlogPost from "@/components/Blog";
+
+
+export default function Home() {
+  return <div>
+    <Hero/>
+    <About/>
+    <FoodCata/>
+    <WhyChooseUs/>
+    <Experience/>
+    <OurChefs/>
+    <BlogPost/>
+    </div>
+  
+}
